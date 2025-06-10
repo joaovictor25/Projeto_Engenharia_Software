63609d97d2dfd922d664251be0aa5bb8400aadb4
JOÃO VICTOR BATISTA CARNEIRO <06004611@acad.unifeso.lan>
2025-06-10 20:14:30 -0300

    Criação do Dashboard

---
+++ sistema_rural/src/app/dashboard/page.tsx
@@ -0,0 +1,35 @@
+import style from "./style.module.css";
+import Image from "next/image";
+import UserImg from "../../../public/assets/user.png";
+
+export default function Page(){
+    return(
+        <>
+        <div className={style.container}>
+            <div className={style.menu}>
+                <div className={style.nav_menu}>
+                    <ul>
+                        <li className={style.home}>Home</li>
+                        <li className={style.cotacao}>Cotação</li>
+                        <li className={style.contato}>Contato</li>
+                    </ul> 
+                </div>
+                <div className={style.user}>
+                    <p>João Victor</p>
+                    <Image src={UserImg} alt="Imagem de perfil" width={50} height={50}/>
+                </div>
+            </div>
+
+            <div className={style.main}>
+                <div className={style.sidebar}>
+                    Barra lateral
+                </div>
+
+                <div className={style.content}>
+                    Conteudo
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
